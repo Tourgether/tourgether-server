3dd71384119fb83212a9571fbee84f3bb3d91d27
ljg0610 <jggy0627@naver.com>
2025-04-03 17:05:27 +0900

    feat: 네이버 연동해제 구현

---
+++ src/main/java/com/tourgether/tourgether/auth/unlink/handler/NaverUnlinkHandler.java
@@ -0,0 +1,44 @@
+package com.tourgether.tourgether.auth.unlink.handler;
+
+import com.tourgether.tourgether.auth.unlink.SocialUnlinkHandler;
+import com.tourgether.tourgether.member.enums.Provider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
+public class NaverUnlinkHandler implements SocialUnlinkHandler {
+
+    private final RestTemplate restTemplate;
+
+    //TODO: yaml설정 후 Value 어노테이션 추가 예정
+    private String clientId;
+
+    //TODO: yaml설정 후 Value 어노테이션 추가 예정
+    private String clientSecret;
+
+    @Override
+    public Provider getProvider() {
+        return Provider.NAVER;
+    }
+
+    @Override
+    public void unlink(String accessToken) {
+        String url = "https://nid.naver.com/oauth2.0/token" +
+                "?grant_type=delete" +
+                "&client_id=" + clientId +
+                "&client_secret=" + clientSecret +
+                "&access_token=" + accessToken;
+
+        try {
+            ResponseEntity<String> respone = restTemplate.postForEntity(url, null, String.class);
+            if (!respone.getStatusCode().is2xxSuccessful()) {
+                throw new RuntimeException("NidaverUnlink failed with status " + respone.getStatusCode());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("NidaverUnlink failed Exception", e);
+        }
+    }
+}
