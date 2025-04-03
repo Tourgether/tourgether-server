a07df78006e0f515387a8c3ff4ec3bbe5a2b4188
ljg0610 <jggy0627@naver.com>
2025-04-03 16:40:14 +0900

    feat: oauth 연동해제 공통 인터페이스

---
+++ src/main/java/com/tourgether/tourgether/auth/unlink/SocialUnlinkHandler.java
@@ -0,0 +1,10 @@
+package com.tourgether.tourgether.auth.unlink;
+
+import com.tourgether.tourgether.member.enums.Provider;
+
+public interface SocialUnlinkHandler {
+    Provider getProvider();
+
+    // 카카오 - userId, 네이버/구글 - accessToken
+    void unlink(String identifier);
+}
