9eb127151eaeef3df5d22f2580013ae0928b577b
wjdtjdgns <person3275@gmail.com>
2025-04-04 00:58:55 +0900

    feat: Attraction, AttractionTranslation, 검색 레포지토리 추가

---
+++ src/main/java/com/tourgether/tourgether/language/repository/LanguageRepository.java
@@ -0,0 +1,8 @@
+package com.tourgether.tourgether.language.repository;
+
+import com.tourgether.tourgether.language.entity.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+}
