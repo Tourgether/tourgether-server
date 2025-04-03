d267459452554afe2a8a645727e05d75ba2bcf9b
ljg0610 <jggy0627@naver.com>
2025-04-03 16:57:48 +0900

    feat: 카카오 연동해제 구현

--- src/main/java/com/tourgether/tourgether/auth/unlink/handler/KakaoUnlinkHandler.java
+++ src/main/java/com/tourgether/tourgether/auth/unlink/handler/KakaoUnlinkHandler.java
@@ -3,7 +3,10 @@ package com.tourgether.tourgether.auth.unlink.handler;
 import com.tourgether.tourgether.auth.unlink.SocialUnlinkHandler;
 import com.tourgether.tourgether.member.enums.Provider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -12,14 +15,35 @@ public class KakaoUnlinkHandler implements SocialUnlinkHandler {
 
     private final RestTemplate restTemplate;
 
+    //TODO: yaml설정 후 Value 어노테이션 추가 예정
+    private String kakaoAdminKey;
 
     @Override
     public Provider getProvider() {
-        return null;
+        return Provider.KAKAO;
     }
 
     @Override
-    public void unlink(String identifier) {
+    public void unlink(String providerId) {
+        String url = "https://kapi.kakao.com/v1/user/unlink";
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "KakaoAK " + kakaoAdminKey);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("target_id_type", "user_id");
+        body.add("target_id", providerId);
+
+        HttpEntity<MultiValueMap<String, String>> rqeust = new HttpEntity<>(body, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(url, rqeust, String.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new RuntimeException("Kakao unlink failed: " + response.getBody());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Kakao unlink failed Exception: " + e.getMessage());
+        }
     }
 }
