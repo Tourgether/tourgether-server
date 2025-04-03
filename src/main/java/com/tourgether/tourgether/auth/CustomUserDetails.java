32e67b1f005084cebf239940ff2ab97783312106
ljg0610 <jggy0627@naver.com>
2025-04-03 13:51:57 +0900

    feat: 인증된 사용자 정보를 주입받기 위한 CustomerUserDetails 구현

---
+++ src/main/java/com/tourgether/tourgether/auth/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.tourgether.tourgether.auth;
+
+import com.tourgether.tourgether.member.enums.Provider;
+import com.tourgether.tourgether.member.enums.Status;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Long memberId;
+    private final String providerId;
+    private final Provider provider;
+    private final String nickname;
+    private final Status status;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return providerId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return status == Status.ACTIVE;
+    }
+}
