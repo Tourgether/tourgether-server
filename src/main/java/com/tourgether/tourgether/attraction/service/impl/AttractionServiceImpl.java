59580a75913575cc5e567154c33b81660d1c0ab3
wjdtjdgns <person3275@gmail.com>
2025-04-08 03:06:24 +0900

    feat: Attraction 서비스 exception 추가

--- src/main/java/com/tourgether/tourgether/attraction/service/impl/AttractionServiceImpl.java
+++ src/main/java/com/tourgether/tourgether/attraction/service/impl/AttractionServiceImpl.java
@@ -4,10 +4,12 @@ import com.tourgether.tourgether.attraction.dto.AttractionDetailResponse;
 import com.tourgether.tourgether.attraction.dto.AttractionSummaryResponse;
 import com.tourgether.tourgether.attraction.dto.LevelDescriptionResponse;
 import com.tourgether.tourgether.attraction.entity.AttractionTranslation;
+import com.tourgether.tourgether.attraction.exception.AttractionTranslationNotFoundException;
 import com.tourgether.tourgether.attraction.repository.AttractionTranslationRepository;
 import com.tourgether.tourgether.attraction.repository.LevelDescriptionRepository;
 import com.tourgether.tourgether.attraction.service.AttractionService;
 import com.tourgether.tourgether.language.entity.Language;
+import com.tourgether.tourgether.language.exception.LanguageNotFoundException;
 import com.tourgether.tourgether.language.repository.LanguageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,9 +29,8 @@ public class AttractionServiceImpl implements AttractionService {
   @Override
   public List<AttractionSummaryResponse> searchAttractions(Long languageId, String keyword) {
 
-    // TODO: custom exception
     Language language = languageRepository.findById(languageId)
-        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 언어입니다."));
+        .orElseThrow(() -> new LanguageNotFoundException("존재하지 않는 언어입니다."));
 
     return translationRepository.searchByKeywordInFields(language, keyword)
         .stream()
@@ -53,13 +54,18 @@ public class AttractionServiceImpl implements AttractionService {
 
     AttractionTranslation attractionTranslation = translationRepository.findByAttractionIdAndLanguageId(
             attractionId, languageId)
-        .orElseThrow(() -> new IllegalArgumentException("해당 언어의 여행지 정보를 찾을 수 없습니다."));
+        .orElseThrow(() -> new AttractionTranslationNotFoundException("해당 언어의 여행지 정보를 찾을 수 없습니다."));
 
     return AttractionDetailResponse.from(attractionTranslation);
   }
 
   @Override
   public List<LevelDescriptionResponse> getAttractionLevelDescriptions(Long translationId) {
+
+    boolean exists = translationRepository.existsById(translationId);
+    if (!exists) {
+      throw new AttractionTranslationNotFoundException("해당 번역 ID의 여행지 정보를 찾을 수 없습니다.");
+    }
 
     return levelDescriptionRepository
         .findByTranslationTranslationId(translationId)
