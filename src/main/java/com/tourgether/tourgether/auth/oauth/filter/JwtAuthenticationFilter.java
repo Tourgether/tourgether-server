1cbd07b819ab99b48c6929b69fe2fd2b7962ddc3
cramfull <tmdgud99@gmail.com>
2025-04-08 01:07:40 +0900

    feat: JwtAuthenticationFilter 진행중

--- src/main/java/com/tourgether/tourgether/auth/oauth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/tourgether/tourgether/auth/oauth/filter/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package com.tourgether.tourgether.auth.oauth.filter;
 
 import com.tourgether.tourgether.auth.service.TokenMappingService;
+import com.tourgether.tourgether.auth.util.HeaderUtil;
 import com.tourgether.tourgether.auth.util.JwtUtil;
+import com.tourgether.tourgether.member.entity.Member;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,46 +21,27 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final HeaderUtil headerUtil;
     private final TokenMappingService tokenMappingService;
 
-    private final String PREFIX_AUTHORIZATION = "Authorization";
-    private final String PREFIX_TOKEN_BEARER = "Bearer ";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // request에서 token 추출
+        String token = headerUtil.resolveToken(request);
 
-        String authorizationHeader = request.getHeader(PREFIX_AUTHORIZATION);
-        validateAuthorizationHeader(request, response, filterChain, authorizationHeader);
-
-        String accessToken = authorizationHeader.substring(7);
-        validateAccessToken(request, response, filterChain, accessToken);
+        if (token == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         try {
-            String tokenCode = jwtUtil.getTokenCodeFromAccessToken(accessToken);
-            Long memberId = tokenMappingService.getMemberIdFromTokenCode(tokenCode);
-            validateMappingTokenCode(request, response, filterChain, memberId);
 
         } catch () {
 
 
         }
 
-    }
-
-
-    private void validateAuthorizationHeader(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, String authorizationHeader) throws ServletException, IOException {
-        if (authorizationHeader == null || !authorizationHeader.startsWith(PREFIX_TOKEN_BEARER)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-    }
-
-    private void validateAccessToken(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, String accessToken) throws ServletException, IOException {
-        if (!jwtUtil.validateAccessToken(accessToken)) {
-            log.warn("유효하지 않은 Access Token");
-            filterChain.doFilter(request, response);
-            return;
-        }
     }
 
     private void validateMappingTokenCode(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, Long memberId) throws ServletException, IOException {
--- src/main/java/com/tourgether/tourgether/auth/util/JwtUtil.java
+++ src/main/java/com/tourgether/tourgether/auth/util/JwtUtil.java
@@ -1,8 +1,11 @@
 package com.tourgether.tourgether.auth.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import java.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,174 +18,176 @@ import java.util.UUID;
 @Component
 public class JwtUtil {
 
-    private final SecretKey accessSecretKey;
-    private final SecretKey refreshSecretKey;
+  private final SecretKey accessSecretKey;
+  private final SecretKey refreshSecretKey;
 
-    private final long accessTokenValidityTime = 60 * 60 * 1000L; // 1시간
-    private final long refreshTokenValidityTime = 14 * 24 * 60 * 60 * 1000L; // 2주
+  private final long accessTokenValidityTime = 60 * 60 * 1000L; // 1시간
+  private final long refreshTokenValidityTime = 14 * 24 * 60 * 60 * 1000L; // 2주
 
-    /**
-     * encoded HS256
-     *
-     * @param accessSecretKey  32Byte random code
-     * @param refreshSecretKey 32Byte random code
-     */
-    public JwtUtil(@Value("spring.jwt.access.secret-key") String accessSecretKey,
-                   @Value("spring.jwt.refresh.secret-key") String refreshSecretKey) {
-        this.accessSecretKey = new SecretKeySpec(
-                accessSecretKey.getBytes(StandardCharsets.UTF_8),
-                Jwts.SIG.HS256.key().build().getAlgorithm());
-        this.refreshSecretKey = new SecretKeySpec(
-                refreshSecretKey.getBytes(StandardCharsets.UTF_8),
-                Jwts.SIG.HS256.key().build().getAlgorithm());
+  /**
+   * encoded HS256
+   *
+   * @param accessSecretKey  32Byte random code
+   * @param refreshSecretKey 32Byte random code
+   */
+  public JwtUtil(@Value("spring.jwt.access.secret-key") String accessSecretKey,
+      @Value("spring.jwt.refresh.secret-key") String refreshSecretKey) {
+    this.accessSecretKey = new SecretKeySpec(
+        accessSecretKey.getBytes(StandardCharsets.UTF_8),
+        Jwts.SIG.HS256.key().build().getAlgorithm());
+    this.refreshSecretKey = new SecretKeySpec(
+        refreshSecretKey.getBytes(StandardCharsets.UTF_8),
+        Jwts.SIG.HS256.key().build().getAlgorithm());
+  }
+
+  /**
+   * AccessToken 생성
+   *
+   * @param tokenCode 서버 내 member id 맵핑 식별자
+   * @return AccessToken
+   */
+  public String generateAccessToken(String tokenCode) {
+    return buildToken(tokenCode, accessSecretKey, accessTokenValidityTime);
+  }
+
+  /**
+   * RefreshToken 생성
+   *
+   * @param tokenCode 서버 내 memberId 맵핑 식별자
+   * @return RefreshToken
+   */
+  public String generateRefreshToken(String tokenCode) {
+    return buildToken(tokenCode, refreshSecretKey, refreshTokenValidityTime);
+  }
+
+  /**
+   * {access, refresh} Token 생성
+   *
+   * @param tokenCode     서버 내 memberId 맵핑 식별자
+   * @param secretKey     {access, refresh} secretKey
+   * @param validityMills {access, refresh} validityTime
+   * @return {access, refresh} Token
+   */
+  private String buildToken(String tokenCode, SecretKey secretKey,
+      long validityMills) {
+    Date now = new Date();
+    Date expiredDate = new Date(now.getTime() + validityMills);
+    return Jwts.builder()
+        .subject(tokenCode)
+        .issuedAt(now)
+        .expiration(expiredDate)
+        .signWith(secretKey)
+        .compact();
+  }
+
+  /**
+   * Token parsing
+   *
+   * @param token     {access, refresh} token
+   * @param secretKey {access, refresh} secretKey
+   * @return jwt token 정보
+   */
+  private Claims parseToken(String token, SecretKey secretKey) {
+    return Jwts.parser()
+        .verifyWith(secretKey)
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  /**
+   * access token 만료 검증
+   *
+   * @param accessToken 사용자의 access token
+   * @return access token 만료 여부
+   */
+  public boolean validateAccessToken(String accessToken) {
+    return validateTokenWithKey(accessToken, accessSecretKey);
+  }
+
+  /**
+   * refresh token 만료 검증
+   *
+   * @param refreshToken 사용자의 refresh token
+   * @return refresh token 만료 여부
+   */
+  public boolean validateRefreshToken(String refreshToken) {
+    return validateTokenWithKey(refreshToken, refreshSecretKey);
+  }
+
+  /**
+   * 토큰 만료 검증
+   *
+   * @param token {access, refresh} token
+   * @param key   {access, refresh} secretKey
+   * @return 토큰 만료 여부
+   */
+  private boolean validateTokenWithKey(String token, SecretKey key) {
+    try {
+      Claims claims = parseToken(token, key);
+      return !isTokenExpired(claims);
+    } catch (JwtException | IllegalArgumentException e) {
+      return false;
     }
+  }
 
-    /**
-     * AccessToken 생성
-     *
-     * @param tokenCode 서버 내 member id 맵핑 식별자
-     * @return AccessToken
-     */
-    public String generateAccessToken(String tokenCode) {
-        return buildToken(tokenCode, accessSecretKey, accessTokenValidityTime);
-    }
+  /**
+   * 토큰 만료 검증
+   *
+   * @param claims 토큰 정보
+   * @return 토큰 만료 여부
+   */
+  private boolean isTokenExpired(Claims claims) {
+    return claims.getExpiration().before(new Date());
+  }
 
-    /**
-     * RefreshToken 생성
-     *
-     * @param tokenCode 서버 내 memberId 맵핑 식별자
-     * @return RefreshToken
-     */
-    public String generateRefreshToken(String tokenCode) {
-        return buildToken(tokenCode, refreshSecretKey, refreshTokenValidityTime);
-    }
+  /**
+   * access token parsing
+   *
+   * @param accessToken 사용자의 access token
+   * @return jwt token 정보
+   */
+  private Claims extractAccessToken(String accessToken) {
+    return parseToken(accessToken, accessSecretKey);
+  }
 
-    /**
-     * {access, refresh} Token 생성
-     *
-     * @param tokenCode      서버 내 memberId 맵핑 식별자
-     * @param secretKey     {access, refresh} secretKey
-     * @param validityMills {access, refresh} validityTime
-     * @return {access, refresh} Token
-     */
-    private String buildToken(String tokenCode, SecretKey secretKey, long validityMills) {
-        Date now = new Date();
-        Date expiredDate = new Date(now.getTime() + validityMills);
-        return Jwts.builder()
-                .subject(tokenCode)
-                .issuedAt(now)
-                .expiration(expiredDate)
-                .signWith(secretKey)
-                .compact();
-    }
+  /**
+   * refresh token parsing
+   *
+   * @param refreshToken 사용자의 refresh token
+   * @return jwt token 정보
+   */
+  private Claims extractRefreshToken(String refreshToken) {
+    return parseToken(refreshToken, refreshSecretKey);
+  }
 
-    /**
-     * Token parsing
-     *
-     * @param token     {access, refresh} token
-     * @param secretKey {access, refresh} secretKey
-     * @return jwt token 정보
-     */
-    private Claims parseToken(String token, SecretKey secretKey) {
-        return Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-    }
+  /**
+   * 서버 내 memberId 맵핑 식별자인 tokenCode 추출
+   *
+   * @param accessToken 사용자의 access token
+   * @return tokenCode
+   */
+  public String getTokenCodeFromAccessToken(String accessToken) {
+    return extractAccessToken(accessToken).getSubject();
+  }
 
-    /**
-     * access token 만료 검증
-     *
-     * @param accessToken 사용자의 access token
-     * @return access token 만료 여부
-     */
-    public boolean validateAccessToken(String accessToken) {
-        return validateTokenWithKey(accessToken, accessSecretKey);
-    }
+  /**
+   * 서버 내 memberId 맵핑 식별자인 tokenCode 추출
+   *
+   * @param refreshToken 사용자의 refresh token
+   * @return tokenCode
+   */
+  public String getTokenCodeFromRefreshToken(String refreshToken) {
+    return extractRefreshToken(refreshToken).getSubject();
+  }
 
-    /**
-     * refresh token 만료 검증
-     *
-     * @param refreshToken 사용자의 refresh token
-     * @return refresh token 만료 여부
-     */
-    public boolean validateRefreshToken(String refreshToken) {
-        return validateTokenWithKey(refreshToken, refreshSecretKey);
-    }
+  /**
+   * tokenCode 위한 Random UUID 생성
+   *
+   * @return tokenCode
+   */
+  public String getRandomKey() {
+    return UUID.randomUUID().toString();
+  }
 
-    /**
-     * 토큰 만료 검증
-     *
-     * @param token {access, refresh} token
-     * @param key   {access, refresh} secretKey
-     * @return 토큰 만료 여부
-     */
-    private boolean validateTokenWithKey(String token, SecretKey key) {
-        try {
-            Claims claims = parseToken(token, key);
-            return !isTokenExpired(claims);
-        } catch (JwtException | IllegalArgumentException e) {
-            return false;
-        }
-    }
-
-    /**
-     * 토큰 만료 검증
-     *
-     * @param claims 토큰 정보
-     * @return 토큰 만료 여부
-     */
-    private boolean isTokenExpired(Claims claims) {
-        return claims.getExpiration().before(new Date());
-    }
-
-    /**
-     * access token parsing
-     *
-     * @param accessToken 사용자의 access token
-     * @return jwt token 정보
-     */
-    private Claims extractAccessToken(String accessToken) {
-        return parseToken(accessToken, accessSecretKey);
-    }
-
-    /**
-     * refresh token parsing
-     *
-     * @param refreshToken 사용자의 refresh token
-     * @return jwt token 정보
-     */
-    private Claims extractRefreshToken(String refreshToken) {
-        return parseToken(refreshToken, refreshSecretKey);
-    }
-
-    /**
-     * 서버 내 memberId 맵핑 식별자인 tokenCode 추출
-     *
-     * @param accessToken 사용자의 access token
-     * @return tokenCode
-     */
-    public String getTokenCodeFromAccessToken(String accessToken) {
-        return extractAccessToken(accessToken).getSubject();
-    }
-
-    /**
-     * 서버 내 memberId 맵핑 식별자인 tokenCode 추출
-     *
-     * @param refreshToken 사용자의 refresh token
-     * @return tokenCode
-     */
-    public String getTokenCodeFromRefreshToken(String refreshToken) {
-        return extractRefreshToken(refreshToken).getSubject();
-    }
-
-    /**
-     * tokenCode 위한 Random UUID 생성
-     *
-     * @return tokenCode
-     */
-    public String getRandomKey() {
-        return UUID.randomUUID().toString();
-    }
 }
