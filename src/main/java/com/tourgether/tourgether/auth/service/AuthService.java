dd2093368707f6d1fcdbf3e06e24390c2da31ffe
cramfull <tmdgud99@gmail.com>
2025-04-09 01:18:03 +0900

    fix: AuthService 컴포넌트 수정, .yml jwt-secretKey 환경 변수로 사용

--- src/main/java/com/tourgether/tourgether/auth/service/AuthService.java
+++ src/main/java/com/tourgether/tourgether/auth/service/AuthService.java
@@ -11,10 +11,10 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class AuthService {
 
