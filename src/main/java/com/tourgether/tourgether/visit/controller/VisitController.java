f12bcafa9c9ccf5b97787a31c0994bb0e7f5c223
ljg0610 <jggy0627@naver.com>
2025-04-25 22:27:42 +0900

    feat: 방문기록 조회 api 추가

--- src/main/java/com/tourgether/tourgether/visit/controller/VisitController.java
+++ src/main/java/com/tourgether/tourgether/visit/controller/VisitController.java
@@ -4,11 +4,17 @@ import com.tourgether.tourgether.auth.CustomUserDetails;
 import com.tourgether.tourgether.common.dto.ApiResult;
 import com.tourgether.tourgether.visit.dto.request.VisitCreateRequest;
 import com.tourgether.tourgether.visit.dto.response.VisitCreateResponse;
+import com.tourgether.tourgether.visit.dto.response.VisitResponse;
 import com.tourgether.tourgether.visit.service.VisitService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +34,15 @@ public class VisitController {
 
     VisitCreateResponse response = visitService.createVisit(userDetails.memberId(), request);
 
+    return ResponseEntity.ok(ApiResult.success(response));
+  }
+
+  @GetMapping
+  public ResponseEntity<ApiResult<Page<VisitResponse>>> getVisitHistory(
+      @AuthenticationPrincipal CustomUserDetails userDetails,
+      @PageableDefault(sort = "visitedAt", direction = Sort.Direction.DESC) Pageable pageable
+  ) {
+    Page<VisitResponse> response = visitService.getVisitHistory(userDetails.memberId(), pageable);
     return ResponseEntity.ok(ApiResult.success(response));
   }
 }
